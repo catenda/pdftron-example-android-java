b2f54c68ec72b30ce599eee32bbd8b17be9d9aaa
Mark Holland <mark.holland@catenda.no>
2021-03-18 17:21:26 +0100

    Init Java project and integrate PDFTron SDK

---
+++ app/src/main/java/no/catenda/pdftronExample/MainActivity.java
@@ -0,0 +1,22 @@
+package no.catenda.pdftronExample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.net.Uri;
+import com.pdftron.pdf.config.ViewerConfig;
+import com.pdftron.pdf.controls.DocumentActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewerConfig config = new ViewerConfig.Builder().openUrlCachePath(this.getCacheDir().getAbsolutePath()).build();
+        final Uri fileLink = Uri.parse("https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_mobile_about.pdf");
+        DocumentActivity.openDocument(this, fileLink, config);
+    }
+}
